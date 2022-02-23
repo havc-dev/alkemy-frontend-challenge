d4cf6c611292c629ff9188995ee47383917d4db0
Adrián Curiel <81223357+havc-dev@users.noreply.github.com>
2022-02-23 16:56:19 -0500

    Create Header.jsx

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+	return (
+		<header className='text-center'>
+			<h1>Foodem Listsum</h1>
+			<h2>An Alkemy Challenge app</h2>
+		</header>
+	);
+};
+
+export default Header;
